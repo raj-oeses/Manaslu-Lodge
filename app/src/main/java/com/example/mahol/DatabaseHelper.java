716254ad4c4c8a371c443679b75121c786321696
WorkOut <ashishrajbanshi40@gmail.com>
2020-05-18 19:26:52 +0545

    checking weather room-number or prices is entered or not

--- app/src/main/java/com/example/mahol/DatabaseHelper.java
+++ app/src/main/java/com/example/mahol/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.mahol;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.ImageButton;
@@ -10,9 +12,9 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static String DATABASE_NAME="Mahol.db";
     public static String TABLE_NAME="Record";
-    public static String COLUMN_1="DATE";
-    public static String COLUMN_2="ROOM_NUMBER";
-    public static String COLUMN_3="PRICE";
+    public static String column_1="DATE";
+    public static String column_2="ROOM_NUMBER";
+    public static String column_3="PRICE";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -30,6 +32,29 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
 
     }
+    public boolean insertData(String roomnumber,int price) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_2, roomnumber);
+        contentValues.put(column_3, price);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1) {
+            return false;
+        }
+
+        else {
+            return true;
+        }
+    }
+
+    public Cursor getData(){
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor res=db.rawQuery(" select * from "+ TABLE_NAME,null);
+        return res;
+
+    }
+
 
 
 
--- app/src/main/java/com/example/mahol/MainActivity.java
+++ app/src/main/java/com/example/mahol/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.mahol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+
+    Spinner room_number;
+    Button enter;
+    EditText price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        room_number = (Spinner) findViewById(R.id.roomnumber);
+        price = (EditText) findViewById(R.id.price);
+
+        enter = (Button) findViewById(R.id.enter);
+        enter.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        check();
+                    }
+                }
+        );
+        //addData();
+
+
     }
+
+    public void check() {
+        if (room_number.getSelectedItem().equals("Room Number") || TextUtils.isEmpty(price.getText().toString())) {
+            Toast.makeText(MainActivity.this, "Something is missing", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = new Intent(this, demoactivity.class);
+            startActivity(intent);
+
+        }
+    }
+
+
+        public void addData() {
+            String roomnumber = String.valueOf(room_number.getSelectedItem());
+            final int roomprice = Integer.parseInt(price.getText().toString());
+            myDb.insertData(roomnumber, roomprice);
+
+        }
+
+
 }
